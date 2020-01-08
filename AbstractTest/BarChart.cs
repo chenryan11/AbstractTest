5b5273975f7cddc2a82598ccdec5635949864068
Ryan <chenryan11@yahoo.com.tw>
2020-01-08 22:03:54 +0800

    first commit

---
+++ AbstractTest/BarChart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace AbstractTest
+{
+    public abstract class BarChart
+    {
+
+        void addBar()
+        {
+
+        }
+
+
+
+        public abstract void plot();
+    }
+}
---
+++ AbstractTest/HorizontalBarChart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractTest
+{
+    class HorizontalBarChart : BarChart
+    {
+        //文字 + **
+        public override void plot()
+        {
+
+            //輸出
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
